8df833eee7d3dd2a600fb1d2fbf37052614c41a4
ahydl <ahydl@hyeyoung.local>
2016-08-17 02:13:39 +0900

    test

---
+++ src/main/java/Sample.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+public class Sample {
+
+    private String testValue = "SONAR";
+
+    public void Test1() {
+        String testValue1 = "";
+        String testValue2 = "TEST";
+        boolean booleanValue = false;
+
+
+        if (booleanValue == true) {
+            System.out.print("########### BOOLEAN VALUE TRUE ########");
+        }
+
+        if (testValue.equals(testValue2.toString())) {
+            System.out.print("####### VALUE EQUAL #####");
+        }
+
+
+    }
+
+
+    public List<String> SampleMethod1() {
+
+        return null;
+    }
+}
