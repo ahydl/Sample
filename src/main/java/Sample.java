314fba03336156326113dfadd1a8d0c014325276
ahydl <ahydl@hyeyoung.local>
2016-08-17 11:57:03 +0900

    test

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -1,4 +1,7 @@
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Sample {
 
@@ -7,20 +10,44 @@ public class Sample {
     public void Test1() {
         String testValue1 = "";
         String testValue2 = "TEST";
-        boolean booleanValue = false;
+        boolean booleanValue = true;
 
 
         if (booleanValue == true) {
-            System.out.print("########### BOOLEAN VALUE TRUE ########");
+
         }
 
-        if (testValue.equals(testValue2.toString())) {
-            System.out.print("####### VALUE EQUAL #####");
+        if (testValue.equals(testValue2.toString()) ==  false) {
+
         }
 
 
     }
 
+
+    public void Test2() {
+
+        Map<String, Object> sampleMap = new HashMap<String, Object>();
+        if(sampleMap == null) {
+
+        }
+
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        try {
+
+        } catch (Exception e) {
+
+        }
+
+        long[] testArr = {0,1,2,3,4,5,6,7,8,9};
+        for(long val : testArr) {
+            if(val % 2 == 0) {
+                continue;
+            }
+        }
+
+    }
 
     public List<String> SampleMethod1() {
 
